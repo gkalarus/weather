a468f7418ec471f4c62a5bc1b143c39b9a5f33fc
Grzegorz <kalarus.g@gmail.com>
2019-01-29 23:34:19 +0100

    Updates

---
+++ precache-manifest.117047b5d228bec1e72cc11e7193123d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6f2f1bad802a5ba57b63619e3926ce4",
+    "url": "/weather/static/media/img.d6f2f1ba.jpeg"
+  },
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "d6842ef40a5324b65f4a",
+    "url": "/weather/static/js/main.d6842ef4.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/weather/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "d6842ef40a5324b65f4a",
+    "url": "/weather/static/css/main.e4d29010.chunk.css"
+  },
+  {
+    "revision": "19ee8f21db20dafedcf83f7723393302",
+    "url": "/weather/index.html"
+  }
+];
